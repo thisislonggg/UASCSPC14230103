dfc3282195e716884a09794f202193aa6a10afb9
Jonathan Christopher Salim <jojonathan192006@gmail.com>
2025-12-17 08:20:13 +0700

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // contoh kalau memang butuh path:
+  // webpack(config) { ... }
+}
 
-export default nextConfig;
+export default nextConfig
